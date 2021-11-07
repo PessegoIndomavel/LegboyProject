cd768cc197a01a84210f2e492d30fd0733f95344
Hugo Forte Sclebin <hugoforte@dcc.ufrj.br>
2021-11-07 18:46:33 -0300

    Small Changes

--- Legboy/Assets/_Scripts/Utility/GenerateVerticalIsleMap.cs
+++ Legboy/Assets/_Scripts/Utility/GenerateVerticalIsleMap.cs
@@ -9,59 +9,70 @@ using UnityEditor;
 public class GenerateVerticalIsleMap : MonoBehaviour
 {
     [SerializeField] private string path = "/_Art/Sprites/Effects/";
+    [SerializeField] private float gradientVariation = 0.5f;
     
-    private int maxHeight = 0;
     public void GenerateVertIsleMap()
     {
         var tilemap = GetComponent<Tilemap>();
 
-        var tiles = GetTiles(tilemap);
+        var tiles = GetTilesGradient(tilemap);
+        //gets a squares gradient mask without any lerp between tiles
 
-        var pixelSize = tilemap.size.x * 16;
+        var xPixelSize = tilemap.size.x * 16;
+        var yPixelSize = tilemap.size.y * 16;
+        //16 is the amount of pixels per unit
 
-        var texture = new Texture2D(pixelSize, pixelSize, TextureFormat.RGB24, false);
+        var texture = new Texture2D(xPixelSize, yPixelSize, TextureFormat.RGB24, false);
+        //creates a new texture
 
         var xSize = tiles.Count * 16;
         var ySize = tiles[0].Count * 16;
-        var y = ySize - 1;
-        var x = 0;
+        
         var upperCellColor = 0f;
         
-        for (; x < xSize; x++)
+        for (var x = 0; x < xSize; x++)
         {
-            for (y = ySize-1; y >= 0; y--)
+            //y starts at the top because the GetTilesGradient function returns a matrix inverted on the y axis
+            for (var y = ySize-1; y >= 0; y--)
             {
-                if((y/16)-1 >= 0) upperCellColor = Mathf.InverseLerp(0, maxHeight, tiles[x / 16][(y/ 16)-1]);
-
-                var currCellColor = Mathf.InverseLerp(0, maxHeight, tiles[x / 16][y / 16]);
+                if ((y / 16) - 1 >= 0) upperCellColor = tiles[x / 16][(y / 16) - 1];
+                //gets the upper square's color, except when the current one is the highest
                 
-                float pixelColor = currCellColor > 0 ? Mathf.Lerp(currCellColor, upperCellColor, ((ySize-1-y)%16)/16f) : pixelColor = 0;
+                var currCellColor = tiles[x / 16][y / 16];
+                //gets current cell color
+                
+                float pixelColor = currCellColor > (gradientVariation*0.9f) ? Mathf.Lerp(currCellColor, upperCellColor, ((ySize-1-y)%16)/16f) : pixelColor = 0f;
+                //lerps between the upper square's color and the current one to create a smooth gradient
                 
                 texture.SetPixel(x,ySize-1-y, new Color(pixelColor, pixelColor, pixelColor, 1));
             }
         }
 
         SaveTexture(texture);
-        SetMatMask("VerticalIsleMap_" + gameObject.scene.handle + ".png");
+        
+        SetMatValues("VerticalIsleMap_" + gameObject.scene.name + ".png", new Vector2(tilemap.size.x, tilemap.size.y));
     }
     
-    private List<List<int>> GetTiles(Tilemap tilemap)
+    private List<List<float>> GetTilesGradient(Tilemap tilemap)
     {
-        var tiles = new List<List<int>>();
+        var tiles = new List<List<float>>();
         
+        //x goes from the first tile's location to the last one's location
         for (var x = tilemap.origin.x; x < (tilemap.origin.x + tilemap.size.x); x++)
         {
-            var tileX = new List<int>();
+            var tileColumn = new List<float>();//initializes a column list at current x position's tile
+            
+            //y goes from the last tile's location to the first one's location
             for (var y = (tilemap.origin.y + tilemap.size.y); y > tilemap.origin.y; y--)
             {
-                var temp = tilemap.HasTile(new Vector3Int(x, y, 0)) ? 1 : 0;
-                if(temp != 0) temp += (y < (tilemap.origin.y + tilemap.size.y)) ? tileX[(tilemap.origin.y + tilemap.size.y) - y - 1] : 0;
+                //if current tile is filled, sets a positive value other than 0f
+                var tileColor = tilemap.HasTile(new Vector3Int(x, y, 0)) ? gradientVariation : 0f;
+                //if current tile is filled and not in the highest line adds the upper tile's value to the current one's value
+                if(tileColor != 0) tileColor += (y < (tilemap.origin.y + tilemap.size.y)) ? tileColumn[(tilemap.origin.y + tilemap.size.y) - y - 1] : 0f;
 
-                tileX.Add(temp);
-                
-                if (temp > maxHeight) maxHeight = temp;
+                tileColumn.Add(tileColor);
             }
-            tiles.Add(tileX);
+            tiles.Add(tileColumn);
         }
         return tiles;
     }
@@ -73,17 +84,21 @@ public class GenerateVerticalIsleMap : MonoBehaviour
         if(!Directory.Exists(dirPath)) {
             Directory.CreateDirectory(dirPath);
         }
-        File.WriteAllBytes(dirPath + "VerticalIsleMap_" + gameObject.scene.handle + ".png", bytes);
+        File.WriteAllBytes(dirPath + "VerticalIsleMap_" + gameObject.scene.name + ".png", bytes);
         print("Vertical isle map generated.");
     }
 
-    private void SetMatMask(string fileName)
+    //updates the renderer's material's values like mask texture and mask scale
+    private void SetMatValues(string fileName, Vector2 maskScale)
     {
         var mat = GetComponent<Renderer>().sharedMaterial;
-        var tex = (Texture2D) AssetDatabase.LoadAssetAtPath("Assets/_Art/Sprites/Effects/" + fileName, typeof(Texture2D));
+        
+        mat.SetVector("_MaskScale", maskScale);
+        
+        var tex = (Texture2D) AssetDatabase.LoadAssetAtPath("Assets" + path + fileName, typeof(Texture2D));
         if (tex == null)
         {
-            print("Error when assigning mask on material. File not found.");
+            print("Error when assigning mask on material. File " + fileName + " not found at Assets" + path + ".");
             return;
         }
         mat.SetTexture("_MaskTex", tex);
